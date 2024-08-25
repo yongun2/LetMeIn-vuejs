2d4270333ad8e024117332f11d8399c949874ef3
yongun2 <djkye0001@naver.com>
2024-08-25 23:44:23 +0900

    네비게이션 기능 추가

--- src/components/common/MainNavHeader.vue
+++ src/components/common/MainNavHeader.vue
@@ -4,15 +4,27 @@
       <template v-slot:activator="{ props }">
         <v-btn icon="mdi-menu" v-bind="props" class="d-sm-none"></v-btn>
       </template>
+
       <v-list>
-        <v-list-item v-for="nav in navs" :key="nav.name">
-          <v-list-item-title>{{ nav.name }}</v-list-item-title>
+        <v-list-item
+          v-for="nav in navs"
+          :key="nav.name"
+          @click="onClickNav(nav.path)"
+        >
+          <v-list-item-title>
+            {{ nav.name }}
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
 
     <v-app-bar-title> LetMeIn </v-app-bar-title>
-    <v-btn class="d-none d-sm-flex" v-for="nav in navs" :key="nav.name">
+    <v-btn
+      class="d-none d-sm-flex"
+      v-for="nav in navs"
+      :key="nav.name"
+      @click="onClickNav(nav.path)"
+    >
       {{ nav.name }}
     </v-btn>
     <v-btn color="light" variant="outlined">로그인</v-btn>
@@ -20,34 +32,37 @@
 </template>
 
 <script setup>
-import { ref, mergeProps } from "vue";
-import { useRoute } from "vue-router";
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import Logo from "@assets/logo.png";
 
+// vue-router
 const route = useRoute();
+const router = useRouter();
 
-const menu = ref(false);
+// nav 눌렀을 때 path로 routing
+const onClickNav = (path) => router.push(path);
 
 const navs = [
   {
     name: "포트폴리오",
-    path: "",
+    path: `/${route.params.username}/portfolio`,
   },
   {
     name: "프로젝트",
-    path: "",
+    path: `/${route.params.username}/project`,
   },
   {
     name: "이력서",
-    path: "",
+    path: `/${route.params.username}/resume`,
   },
   {
     name: "블로그",
-    path: "",
+    path: `/${route.params.username}/blog`,
   },
   {
     name: "Contact",
-    path: "",
+    path: `/${route.params.username}/contact`,
   },
 ];
 </script>
