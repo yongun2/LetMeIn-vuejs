95666134e3417da63f5dfdf6741a14285a943203
yongun2 <djkye0001@naver.com>
2024-08-25 23:33:06 +0900

    페이지 라우팅 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,50 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { Home } from "@pages/index";
+import {
+  Blog,
+  Contact,
+  Home,
+  Portfolio,
+  Project,
+  Resume,
+  User,
+} from "@pages/index";
 
 const routes = [
   {
     path: "",
     component: Home,
+  },
+  {
+    path: "/:username",
+    component: User,
+    redirect: { name: "Portfolio" },
+    children: [
+      {
+        path: "portfolio",
+        name: "Portfolio",
+        component: Portfolio,
+      },
+      {
+        path: "project",
+        name: "Project",
+        component: Project,
+      },
+      {
+        path: "resume",
+        name: "Resume",
+        component: Resume,
+      },
+      {
+        path: "blog",
+        name: "Blog",
+        component: Blog,
+      },
+      {
+        path: "contact",
+        name: "Contact",
+        component: Contact,
+      },
+    ],
   },
 ];
 
